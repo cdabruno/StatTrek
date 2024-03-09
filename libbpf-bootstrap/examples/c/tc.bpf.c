2c647297de7a5a36c98665c072962c03bc84e963
cdabruno <brunocalmx@gmail.com>
2024-03-09 19:05:21 -0300

    adding tc program

---
+++ libbpf-bootstrap/examples/c/tc.bpf.c
@@ -0,0 +1,43 @@
+#include <vmlinux.h>
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define TC_ACT_OK 0
+#define ETH_P_IP  0x0800 /* Internet Protocol packet	*/
+
+SEC("tc")
+int tc_ingress(struct __sk_buff *ctx)
+{
+	void *data_end = (void *)(__u64)ctx->data_end;
+	void *data = (void *)(__u64)ctx->data;
+	struct ethhdr *l2;
+	struct iphdr *l3;
+
+	if (ctx->protocol != bpf_htons(ETH_P_IP))
+		return TC_ACT_OK;
+
+	l2 = data;
+	if ((void *)(l2 + 1) > data_end)
+		return TC_ACT_OK;
+
+	l3 = (struct iphdr *)(l2 + 1);
+	if ((void *)(l3 + 1) > data_end)
+		return TC_ACT_OK;
+
+
+    bpf_printk("The IP address is %d\n", l3->daddr & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->daddr >> 8) & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->daddr >> 16) & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->daddr >> 24) & 0xFF);
+
+    bpf_printk("The IP address is %d\n", l3->saddr & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->saddr >> 8) & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->saddr >> 16) & 0xFF);
+    bpf_printk("The IP address is %d\n", (l3->saddr >> 24) & 0xFF);
+
+	bpf_printk("Got IP packet: tot_len: %d, ttl: %d", bpf_ntohs(l3->tot_len), l3->ttl);
+	return TC_ACT_OK;
+}
+
+char __license[] SEC("license") = "GPL";

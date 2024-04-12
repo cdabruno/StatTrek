afdfb82154dfc147482b882306b2d926e67a4ec0
Bruno Correa <brunocalmx@gmail.com>
2024-04-12 18:09:03 -0300

    progress

---
+++ nodeMetrics.py
@@ -0,0 +1,27 @@
+import subprocess
+import time
+import json
+
+metricsOutput = open("nodeMetrics.txt", "a")  # append mode
+
+minikubeConsumption = subprocess.check_output("minikube ssh && top", shell=True).decode("utf-8")
+exit()
+
+while(True):
+    # data capture
+    timestamp = time.clock_gettime_ns(time.CLOCK_BOOTTIME)
+    nodeConsumption = subprocess.check_output("kubectl top node", shell=True).decode("utf-8")
+    podConsumption = subprocess.check_output("kubectl top pods", shell=True).decode("utf-8")
+
+    # data persistence
+
+    metricsOutput.write("["+str(timestamp)+"] - Node snapshot\n\n")
+
+    metricsOutput.write("NodeConsumption\n")
+    metricsOutput.write(str(nodeConsumption)+"\n")
+
+    metricsOutput.write("PodsConsumption\n")
+    metricsOutput.write(str(podConsumption) + "\n")
+    time.sleep(1)
+
+metricsOutput.close()
